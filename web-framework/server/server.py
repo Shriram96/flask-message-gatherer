a56e7baee3e13a517627ef5a1357dcacac84d309
Shriram Ravi <shriramravi96@gmail.com>
2022-04-11 18:22:48 -0400

    Supress sqlalchemy warning

--- web-framework/server/server.py
+++ web-framework/server/server.py
@@ -14,6 +14,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = token_urlsafe(16)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 
 FILE_PATH = "messages.csv"
